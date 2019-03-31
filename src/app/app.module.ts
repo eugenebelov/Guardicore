09208ea6db0736b1bf0388172a228610162bc692
eugenebelov <eugene.beloff@gmail.com>
2019-04-01 02:06:01 +0300

    restoring app.module, component, css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { PolygonsListComponent } from './polygons-list/polygons-list.component';
+import { PolygonItemComponent } from './polygons-list/polygon-item/polygon-item.component';
+import { PolygonMakerComponent } from './polygon-maker/polygon-maker.component';
+import { VertexEditComponent, NumberDirective } from './polygons-list/vertex-edit/vertex-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PolygonsListComponent,
+    PolygonItemComponent,
+    PolygonMakerComponent,
+    VertexEditComponent,
+    NumberDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
