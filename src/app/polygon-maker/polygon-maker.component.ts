6a1f6a530bda982e66b36199dc5baac1aa1f5bd2
Yevhen <yevhen.bilov@sigma.software>
2019-04-01 20:06:19 +0300

    fix editing and reset poly's

--- src/app/polygon-maker/polygon-maker.component.ts
+++ src/app/polygon-maker/polygon-maker.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Vertex } from '../vertex.interface';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Polygon, Vertex } from '../vertex.interface';
 import * as store from '../store';
 import { Add } from '../store/actions';
-import { fromEvent } from 'rxjs';
-import { pairwise, switchMap, takeUntil } from 'rxjs/operators';
+import { fromEvent, Observable } from 'rxjs';
+import { pairwise } from 'rxjs/operators';
+import { PolygonState } from '../store/reducers/polygons.reducer';
 
 @Component({
   selector: 'polygon-maker',
   templateUrl: './polygon-maker.component.html',
   styleUrls: ['./polygon-maker.component.scss']
 })
-export class PolygonMakerComponent implements OnInit {
+export class PolygonMakerComponent implements AfterViewInit {
 
-  constructor(private store: Store<store.PolyState>) { }
+  private data$: Observable<any>;
+  constructor(private store: Store<store.PolyState>) {
+    this.data$ = this.store.pipe(select('polygons'));
+  }
 
   @ViewChild('vertexes') vertexesInput: ElementRef;
   @ViewChild('canvas') canvas: ElementRef;
@@ -21,9 +25,6 @@ export class PolygonMakerComponent implements OnInit {
   private cx: CanvasRenderingContext2D;
   private polygon: Array<any> = [];
   private polygonReady: boolean = false;
-
-  ngOnInit() {
-  }
 
   ngAfterViewInit() {
     // get the context
@@ -40,35 +41,50 @@ export class PolygonMakerComponent implements OnInit {
     this.cx.strokeStyle = '#000';
     this.cx.fillStyle = '#00aacc';
 
-    const eve$ = fromEvent(this.canvas.nativeElement, 'click');
-
-    eve$.pipe(
+    fromEvent(this.canvas.nativeElement, 'click').pipe(
       pairwise()
-    ).subscribe( ([start, end]: [MouseEvent, MouseEvent]) => {
-      if (!this.polygonReady) {
-        this.polygon.push({x: start.layerX, y:start.layerY}, {x:end.layerX, y:end.layerY});
+    ).subscribe( ([start, end]: [MouseEvent, MouseEvent]) => this.drawPolygon(start, end));
 
-        if (this.polygon.length) {
-          const firstPoint = this.polygon[0];
-          const lastPoint = this.polygon[this.polygon.length - 1];
+    this.data$.subscribe((state: PolygonState) => {
+      if (state) {
+        this.cx.clearRect(0, 0, 400, 300);
+        state.polygons.forEach((polygon) => {
+         this.cx.beginPath();
+          this.cx.moveTo(polygon.vertexes[0].x, polygon.vertexes[0].y); // from
 
-          if (this.isPointClose(firstPoint, lastPoint)) {
-            this.drawOnCanvas({x: start.layerX, y:start.layerY}, {x:firstPoint.x, y:firstPoint.y});
-            this.fillPoly();
-            this.addPoly();
-            this.polygonReady = true;
-            this.polygon.length = 0;
-          } else {
-            this.drawOnCanvas({x: start.layerX, y:start.layerY}, {x:end.layerX, y:end.layerY});
-          }
-        }
-      } else {
-        this.polygonReady = false;
+          polygon.vertexes.forEach(v => {
+            this.cx.lineTo(v.x, v.y);
+            this.cx.stroke();
+          })
+        })
       }
     })
   }
 
-  private fillPoly() {
+  private drawPolygon(start: MouseEvent, end: MouseEvent): void {
+    if (!this.polygonReady) {
+      this.polygon.push({x: start.layerX, y:start.layerY}, {x:end.layerX, y:end.layerY});
+
+      if (this.polygon.length) {
+        const firstPoint = this.polygon[0];
+        const lastPoint = this.polygon[this.polygon.length - 1];
+
+        if (this.isPointClose(firstPoint, lastPoint)) {
+          this.drawOnCanvas({x: start.layerX, y:start.layerY}, {x:firstPoint.x, y:firstPoint.y});
+          this.fillPoly();
+          this.dispatchPolygonToStore();
+          this.polygonReady = true;
+          this.polygon.length = 0;
+        } else {
+          this.drawOnCanvas({x: start.layerX, y:start.layerY}, {x:end.layerX, y:end.layerY});
+        }
+      }
+    } else {
+      this.polygonReady = false;
+    }
+  }
+
+  private fillPoly(): void {
     const shape = this.polygon.slice(0);
     let curCoord = shape.shift();
     this.cx.beginPath();
@@ -91,10 +107,7 @@ export class PolygonMakerComponent implements OnInit {
     return (X + Y < R);
   }
 
-  private drawOnCanvas(
-    prevPos: { x: number; y: number },
-    currentPos: { x: number; y: number }
-  ) {
+  private drawOnCanvas( prevPos: { x: number; y: number }, currentPos: { x: number; y: number } ): void {
     // incase the context is not set
     if (!this.cx) {
       return;
@@ -115,7 +128,7 @@ export class PolygonMakerComponent implements OnInit {
     }
   }
 
-  private addPoly(): void {
+  private dispatchPolygonToStore(): void {
     const vertexes: Array<Vertex> = [...this.polygon];
     this.store.dispatch(new Add({ polygon: { vertexes: vertexes } }));
   }
